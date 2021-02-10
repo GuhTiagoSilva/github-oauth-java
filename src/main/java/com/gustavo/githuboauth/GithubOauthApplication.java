15745d68f609e19ced25b6474ad7aafbf8fec9ae
GuhTiagoSilva <gustavo.tiago.gts@outlook.com>
2021-02-10 20:51:15 -0300

    Github Oauth Implementation

--- src/main/java/com/gustavo/githuboauth/GithubOauthApplication.java
+++ src/main/java/com/gustavo/githuboauth/GithubOauthApplication.java
@@ -1,11 +1,23 @@
 package com.gustavo.githuboauth;
 
+import java.security.Principal;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableOAuth2Sso
+@RestController
 public class GithubOauthApplication {
 
+	@GetMapping("/")
+	public String message(Principal principal) {
+		return "Hi " + principal.getName() + " welcome to GithubOauthApplication";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(GithubOauthApplication.class, args);
 	}
